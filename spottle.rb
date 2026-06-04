9bbaabe2284b15646b72c92e62949ff72be3163c
James Tombleson <luther38@gmail.com>
2026-06-04 14:58:58 -0700

    version bump

--- spottle.rb
+++ spottle.rb
@@ -5,16 +5,16 @@ class Spottle < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://releases.jamestombleson.com/releases/spottle/3.1.2/spottle_Darwin_x86_64.tar.gz"
-      sha256 "8ecd07e987df0aa8cdbcf98654573702d4c24da4f17439a8f2d053e2b776d682"
+      url "https://releases.jamestombleson.com/releases/spottle/3.2.0/spottle_Darwin_x86_64.tar.gz"
+      sha256 "79a10231235b00f89dcdc44036eeb2a12025630747c290b5337fefcff7bdb84c"
 
       def install
         bin.install "spottle"
       end
     end
     if Hardware::CPU.arm?
-      url "https://releases.jamestombleson.com/releases/spottle/3.1.2/spottle_Darwin_arm64.tar.gz"
-      sha256 "01c0e130fd6f3b8c133995f6e49fb6de86af121445def0159fe7c4f45fa92d27"
+      url "https://releases.jamestombleson.com/releases/spottle/3.2.0/spottle_Darwin_arm64.tar.gz"
+      sha256 "b4057f254733c5c9b63ae7cc90ba85f3bb15edd4e5c2af0ecf1d4c361d4325ea"
 
       def install
         bin.install "spottle"
@@ -25,8 +25,8 @@ class Spottle < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://releases.jamestombleson.com/releases/spottle/3.1.2/spottle_Linux_x86_64.tar.gz"
-        sha256 "a5f935f3fe1b75fa932029214fa9f4748d4bdb6f12513097adfb91b17b68e7c5"
+        url "https://releases.jamestombleson.com/releases/spottle/3.2.0/spottle_Linux_x86_64.tar.gz"
+        sha256 "893072d1e0fe90c84cdcf823774fecb27b6ddad10dd27be3edd696dc52994821"
 
         def install
           bin.install "spottle"
@@ -35,8 +35,8 @@ class Spottle < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://releases.jamestombleson.com/releases/spottle/3.1.2/spottle_Linux_arm64.tar.gz"
-        sha256 "81a7972c2dc35e8cccb387478a25b05a65cd60940cb5cc9b4572d2765cc69647"
+        url "https://releases.jamestombleson.com/releases/spottle/3.2.0/spottle_Linux_arm64.tar.gz"
+        sha256 "bebd57854e60444ef629e31a184ed2cfabf353a99d8e28eaf14e168f074eecbd"
 
         def install
           bin.install "spottle"
